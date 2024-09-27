6a8a3e0497eae3b514e82df4953ab6dcc95edb3a
HOCINE Mohand Ouidir <hocinidir26@gmail.com>
2024-09-27 17:44:42 +0100

    DOM Tree traversal

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const document = dom.window.document;
 */
 /*-----------------------------------------------------------SELECTING ELEMENT IN THE DOM-------------------------------------------------------------------------------*/
 
+
+
+
+/*
 // Get element by id
 const mainHeader = document.getElementById('main_header');
 console.log(mainHeader);
@@ -25,7 +29,7 @@ const listItems_byTagName = document.getElementsByTagName('li');
 console.log(listItems_byTagName);
 
 // querySelector (by passing a tag name) - returns the first element that matches the query
-// const firstDivInMyCode = document.querySelector('div');
+// const firstDivInMyCode = document.querySelector('div');  
 // console.log(firstDivInMyCode);
 
 // querySelectorAll (by passing a tag name) - returns all elements that match the query
@@ -44,8 +48,14 @@ for (let i = 0; i <listItems.length; i++){
     listItems[i].style.color = 'blue';
 }
 
+*/
+
+
+
 /*-----------------------------------------------------------CREATING ELEMENT IN THE DOM-------------------------------------------------------------------------------*/
 
+
+/*
 const ul = document.querySelector('ul');
 const li = document.createElement('li');
 
@@ -55,10 +65,46 @@ li.setAttribute ('class',  'list_item');
 li.style.color = 'green';
 li.style.fontWeight = 'bold';
 
-
-
 ul.appendChild(li); 
 
+*/
 
 
+
+/*------------------------------------------------------------TRAVESING THE DOM TREE----------------------------------------------------------------------------------*/
+
+let ul = document.querySelector('ul');
+console.log(ul);
+/*
+// get the parent of the ul element, we have two ways to do that 
+// 1- using parentNode property
+console.log(ul.parentNode);
+// 2 - using parentElement property
+console.log(ul.parentElement);
+// the diffrence between parentNode and parentElement is that parentNode can return any node type while parentElement returns only elements
+
+*/
+
+let html = document.documentElement; // this returns the entire html document
+console.log(html); 
+
+
+// get the children of the ul element
+console.log(ul.childNodes); // this returns a node list of all the children of the ul element
+// get the first child and the last child of the ul elemnt
+console.log(ul.firstChild);
+console.log(ul.lastChild); 
+// this returns a list of all the children of the ul element that are elements as text 
+// now lete's get the first and last element of the ul element as elements and not as text so we can use children , firstElementChild and lastElementChild properties
+console.log(ul.children);
+console.log(ul.firstElementChild);
+console.log(ul.lastElementChild);
+
+// siblings nodes traversal
+console.log(ul.previousSibling);
+console.log(ul.nextSibling);
+// this return nodes as text so we can use previousElementSibling and nextElementSibling to get the siblings as elements
+
+console.log(ul.previousElementSibling); // h1 element because it is the previous sibling of the ul element
+console.log(ul.nextElementSibling); // nuul because there is no next sibling for the ul element
 
